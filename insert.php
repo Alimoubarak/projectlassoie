7490aabfb1d4af08f839760a3d505506c7e9ac84
Hosny Mubarak <seydel7ob@gmail.com>
2022-05-19 19:43:52 +0200

    add connexion

--- insert.php
+++ insert.php
@@ -2,5 +2,32 @@
 
 include "connexion.php";
 
+if(isset($_GET["nom"]) && isset($_GET["prenom"]) && isset($_GET["tel"]) && isset($_GET["email"]) && isset($_GET["gender"]) )
+
+
+{
+
+
+$nom = $_GET ["nom"];
+$prenom = $_GET ["prenom"];
+$tel = $_GET ["tel"];
+$email = $_GET ["email"];
+$gender = $_GET ["gender"];
+
+
+
+$req=$mysqli_query($link, "insert into user(nom,prenom,tel,email,gender) values ('$nom','$prenom','$tel','$email','$gender' ");
+
+if ($req)
+{
+    echo "inertion effectuée avec succès";
+}
+else 
+{
+    echo "erreur d'insertion";
+}
+
+
+}
 
 ?>
